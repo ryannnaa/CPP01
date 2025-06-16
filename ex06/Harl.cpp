a6638ef4779714da3737e81bde46007ba501dd27
Tian Sheng Ryan Tan <tiatan@c2r1s7.42singapore.sg>
2025-06-16 19:18:37 +0800

    feat(ex05-06): finished harl & harlFilter

---
+++ ex06/Harl.cpp
@@ -0,0 +1,67 @@
+#include "Harl.hpp"
+#include <iostream>
+
+static int levelIndex(const std::string& level)
+{
+    const std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+            return (i);
+    }
+    return (-1);
+}
+
+Harl::Harl()
+{}
+
+Harl::~Harl()
+{}
+
+void Harl::complain(std::string level)
+{
+    int index = levelIndex(level);
+    void (Harl::*functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    switch (index)
+    {
+    case 0:
+        (this->*functions[0])();
+        std::cout << std::endl;
+    case 1:
+        (this->*functions[1])();
+        std::cout << std::endl;
+    case 2:
+        (this->*functions[2])();
+        std::cout << std::endl;
+    case 3:
+        (this->*functions[3])();
+        break;
+    
+    default:
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+}
+
+void Harl::debug()
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error()
+{
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
